98955090caebc5b53ebced6f1fb383d35dbcb569
Wiktor Szczeszek <wiktor.szczeszek@wercom.pl>
2026-07-01 15:17:44 +0200

    chore(fonts): remove unused FluentUI icon font

--- src/Aplication/GlobalUsings.cs
+++ src/Aplication/GlobalUsings.cs
@@ -3,4 +3,3 @@ global using Aplication.Models;
 global using Aplication.Pages;
 global using Aplication.Services;
 global using Aplication.Utilities;
-global using Fonts;
--- src/Aplication/MauiProgram.cs
+++ src/Aplication/MauiProgram.cs
@@ -16,7 +16,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("SegoeUI-Semibold.ttf", "SegoeSemibold");
-                fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
             });
 
 #if DEBUG
